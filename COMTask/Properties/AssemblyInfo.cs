02c25468f99cfd1421304c4dda79b2068dd0d0d5
dahall <dahall@byu.net>
2021-07-29 14:39:06 -0600

    Updated for VS 2019 and 2022. Reversioned to 2.1.0.

--- COMTask/Properties/AssemblyInfo.cs
+++ COMTask/Properties/AssemblyInfo.cs
@@ -25,8 +25,8 @@ using System.Runtime.InteropServices;
 // Major Version Minor Version Build Number Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
 
 // The ActivationOption attribute indicates whether the component will be activated within the caller's process. You can set
 // Activation.Option to Library or to Server.
